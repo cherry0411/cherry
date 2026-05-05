9686d5119d921d740665ecfe430cff02b59333b1
任重远 <leavingys@gmail.com>
2026-05-05 16:26:57 +0800

    perf: 10 instances, 1000 conns, 15000 routing nodes

--- spider/ecosystem.config.js
+++ spider/ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
     apps: [{
         name: 'spider',
         script: './cherry-spider.js',
-        instances: parseInt(process.env.SPIDER_INSTANCES || '6'),
+        instances: parseInt(process.env.SPIDER_INSTANCES || '10'),
         exec_mode: 'fork',
         increment_var: 'SPIDER_PORT',
         env: {
             SPIDER_API_URL: process.env.SPIDER_API_URL || 'http://127.0.0.1:5070',
             SPIDER_INSTANCE_PREFIX: process.env.SPIDER_INSTANCE_PREFIX || 'js',
-            SPIDER_MAX_CONN: '600',
-            SPIDER_MAX_NODES: '5000',
+            SPIDER_MAX_CONN: '1000',
+            SPIDER_MAX_NODES: '15000',
             SPIDER_TIMEOUT: '5000',
             SPIDER_DEDUP_SIZE: '200000',
             SPIDER_PORT: 20003,
